6148bbdb43019528311a9579ef2c1d3adc5abc03
ANURAG-PATHAK <pathakanurag605@gmail.com>
2024-07-14 16:21:04 +0530

    chore: Add ESLint, Next.js, and PostCSS configuration files

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+export const publicRoutes = [
+    "/",
+]
+
+export const authRoutes = [
+    "/auth/login",
+    "/auth/register",
+]
+
+export const apiRoutesPrefix = "/api/auth"
+
+export const DEFAULT_LOGIN_REDIRECT = "/settings" 
